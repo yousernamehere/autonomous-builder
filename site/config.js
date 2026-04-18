da6599aeaab4fb8217d279bc3df806d7e2cbee2d
Jeramie Hicks <jeramieshicks@gmail.com>
2026-04-17 20:30:06 -0400

    Make /site configurable (copy + embed)

---
+++ site/config.js
@@ -0,0 +1,8 @@
+// Edit these values, then redeploy.
+window.LANDING_CONFIG = {
+	headline: "Get your first 10 qualified leads in 7 days (without ads)",
+	subhead: "A simple outbound + landing loop you can run in 60 minutes/day.",
+	ctaText: "Get the 1-page playbook",
+	// Paste your form embed HTML into formEmbedHtml.
+	formEmbedHtml: "",
+};
